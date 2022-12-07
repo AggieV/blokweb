78a14712b53a5b7220dc634eef1c70c129200704
AggieV <117912778+AggieV@users.noreply.github.com>
2022-12-07 09:26:34 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,9 @@
 // JavaScript Document
 console.log("hallo");
 
+/*algemeen*/
+
 /*moviepass*/
-/*het hamburgermenu open en dicht klappen*/
-
-// var hetHamburgerMenu = document.querySelector("header nav ul li:nth-of-type(3)");
-// console.log("hamburgermenu");
-
-// hetHamburgerMenu.addEventListener("click", klapMenuOpenEnDicht);
-
-// function klapMenuOpenEnDicht(){
-//     hetHamburgerMenu.classList.toggle("menuopenendichtklappen");
-// }
 
 var hetHamburgerMenu = document.querySelector("header > button");
 var nav = document.querySelector("header nav");
@@ -33,16 +25,35 @@ function klapMenuDicht() {
     nav.classList.add("menudichtklappen");
 }
 
+/*dropdown veelgestelde vragen */
+
+let deMenuButtons = document.querySelectorAll("section:nth-child(8), section:nth-child(9), section:nth-child(10) > button");
+let deMenuPs = document.querySelectorAll("section:nth-child(8), section:nth-child(9), section:nth-child(10) > p");
+
+deMenuButtons.forEach(deMenuButton => {
+	deMenuButton.addEventListener("click", toggleMenu);
+});
+
+deMenuPs.forEach(deMenuPs => {
+	deMenuPs.addEventListener("click", toggleMenu);
+});
+
+function toggleMenu() {
+	let deButtonWaaropGekliktIs = this; 
+	let deSectionWaarDeButtonInZit = deButtonWaaropGekliktIs.closest("section");
+	deSectionWaarDeButtonInZit.classList.toggle("open");
+}
+
 
 /*maak knop van oranje naar blauw en weer terug*/
 
-var deEersteButton = document.querySelector("section:nth-of-type(3) button:first-child");
-console.log("deEersteButton");
+// var deEersteButton = document.querySelector("section:nth-of-type(3) button:first-child");
+// console.log("deEersteButton");
 
-deEersteButton.addEventListener("click", wordBlauw);
+// deEersteButton.addEventListener("click", wordBlauw);
 
-function wordBlauw(){
-    deEersteButton.classList.toggle("blauwworden");
-}
+// function wordBlauw(){
+//     deEersteButton.classList.toggle("blauwworden");
+// }
 
 /*trailers*/
