3d2aa7887cd3aba4c0e53367adacfc5406ec6366
AggieV <117912778+AggieV@users.noreply.github.com>
2023-11-09 17:53:21 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,12 @@
 // // JavaScript Document
 // console.log("Ola!");
 
-document.addEventListener('DOMContentLoaded', function() {
-  const overlay = document.getElementById('overlay-site');
+//hide zero state
 
-  // Show the overlay when the page is first loaded
-  overlay.style.display = 'flex';
-  overlay.classList.add('overlay-visible');
-  overlay.classList.add('overlay-site');
-
-  // Close the overlay when the user clicks anywhere
-  overlay.addEventListener('click', function() {
-  overlay.classList.remove('overlay-visible');
-    overlay.style.display = 'none';
-  });
+var zeroStateButton = document.getElementById('hide-zero-state');
+  zeroStateButton.addEventListener('click', function() {
+  zeroStateButton.style.display = 'none';
+  console.log('Button1 clicked!');
 });
 
 
@@ -49,18 +42,12 @@ const markers = document.querySelectorAll('.marker');
 const overlay = document.querySelector('.overlay');
 const markerTitleElement = document.querySelector('.marker-title');
 const markerImage = document.querySelector('.marker-image');
-// const markerImageSchicht = document.querySelector('.marker-image2');
 const markerText = document.querySelector('.marker-text');
 const closeButton = document.querySelector('.close-button');
 
 markers.forEach((marker, index) => {
   marker.addEventListener('click', () => appear(index));
 });
-
-
-// markerImageSchicht.addEventListener('click', function() {
-//   this.classList.toggle('paused');
-// });
 
 //close overlays op landkaart
 
@@ -92,26 +79,6 @@ function disappear() {
 
 //toon/verberg paasei op overlay
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   const markerImage = document.querySelector('.marker-image');
-//   const markerImage2 = document.querySelector('.marker-image2');
-//   const markerSound = document.getElementById('markerSound');
-
-//   // Add a click event listener to marker-image
-//   markerImage.addEventListener('click', function() {
-//       // Toggle the display property to show/hide marker-image2
-//       markerImage2.style.display = (markerImage2.style.display === 'none' || markerImage2.style.display === '') ? 'block' : 'none';
-
-//       // Play the sound when marker-image2 is displayed
-//       if (markerImage2.style.display === 'block') {
-//           markerSound.play();
-//       } else {
-//           markerSound.pause();
-//           markerSound.currentTime = 0; // Reset the audio to the beginning when hiding marker-image2
-//       }
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
   const markerImages = document.querySelectorAll('.marker-image');
   const markerImage2s = document.querySelectorAll('.marker-image2');
@@ -123,23 +90,24 @@ document.addEventListener('DOMContentLoaded', function() {
       // Get the marker index from the custom attribute
       const markerIndex = this.dataset.markerIndex;
 
-      // Hide all marker-image2 elements first
-      markerImage2s.forEach(markerImage2 => {
-        markerImage2.style.display = 'none';
-      });
-
-      // Show the selected marker-image2
+      // Toggle the display property of the corresponding marker-image2
       const selectedMarkerImage2 = document.querySelector(`.marker-image2[data-marker-index="${markerIndex}"]`);
       if (selectedMarkerImage2) {
-        selectedMarkerImage2.style.display = 'block';
+        const isMarkerImage2Visible = selectedMarkerImage2.style.display === 'block';
+        markerImage2s.forEach(markerImage2 => {
+          markerImage2.style.display = 'none'; // Hide all marker-image2 elements
+        });
+        if (!isMarkerImage2Visible) {
+          selectedMarkerImage2.style.display = 'block'; // Show the selected marker-image2
+          markerSound.play(); // Play the sound when marker-image2 is displayed
+        } else {
+          markerSound.pause(); // Pause the sound when marker-image2 is hidden
+          markerSound.currentTime = 0; // Reset the audio to the beginning
+        }
       }
-
-      // Play the sound when marker-image2 is displayed
-      markerSound.play();
     });
   });
 });
-
 
 
 //sounds//
